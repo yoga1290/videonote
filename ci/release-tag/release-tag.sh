36e2588473e4ab5d2c4a16d75050c8958f9bfb60
Youssef Gamil <yoga1290@gmail.com>
2026-01-18 00:28:20 -0800

    fixes workflow

--- ci/release-tag/release-tag.sh
+++ ci/release-tag/release-tag.sh
@@ -2,4 +2,4 @@ CI_PATH_TAG='./ci/tag-version/tag-version.sh'
 
 NEW_VERSION=$(bash $CI_PATH_TAG);
 git tag $NEW_VERSION;
-git push;
+git push --tags;
--- ci/tag-version/tag-version.sh
+++ ci/tag-version/tag-version.sh
@@ -4,7 +4,7 @@ REMOTE_ORIGIN="https://${GITHUB_REPOSITORY_OWNER}:${GITHUB_TOKEN}@github.com/${G
 git --version 2>/dev/null 1>/dev/null || apk add git
 
 git remote set-url origin $REMOTE_ORIGIN;
-git pull origin --tags;
+git pull origin --tags 2>/dev/null 1>/dev/null;
 
 YYYY=$(date +%Y)
 TAG_COUNT=$(git tag -l | grep -o "${YYYY}" | wc -l)
