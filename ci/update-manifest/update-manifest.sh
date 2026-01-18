96ec3077e233958be319f2a8647c88433f799bee
Youssef Gamil <yoga1290@gmail.com>
2026-01-18 00:41:38 -0800

    CI

--- ci/update-manifest/update-manifest.sh
+++ ci/update-manifest/update-manifest.sh
@@ -3,6 +3,7 @@ CI_PATH_TAG='./ci/tag-version/tag-version.sh'
 
 MANIFEST_SRC='./ci/update-manifest/manifest.json';
 MANIFEST_DIST='./dist/manifest.json';
+mkdir -p './dist';
 
 TAG_VERSION=$(sh $CI_PATH_TAG 2>/dev/null);
 
